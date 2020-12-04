edc33d29b762791ee90c0226f785e1d570158e94
JiyaJigyasa <Jiyajigyasa1@gmail.com>
2020-12-04 16:17:21 +0530

    second commit

---
+++ WebAppStudent1/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppStudent1.Models
+{
+    public class Student
+    {
+        public int SId { get; set; }
+        public string SName { get; set; }
+        public DateTime SDob { get; set; }
+        public int SContact { get; set; }
+    }
+}
